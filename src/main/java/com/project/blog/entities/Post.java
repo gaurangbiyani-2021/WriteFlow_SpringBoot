266cc82525eaf3c66847b8f6a0fa1fb308a2fe60
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-12-25 16:09:08 +0530

    post class

---
+++ src/main/java/com/project/blog/entities/Post.java
@@ -0,0 +1,31 @@
+package com.project.blog.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="post")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer postId;
+	
+	@Column(name="post_title",nullable=false,length=100)
+	private String title;
+	private String content;
+	private String imageName;
+	private Date addedDate;
+}
