4b74e47103c59f686ea61142fabd4942db0bdfc3
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-12-25 16:36:17 +0530

    error resolved

--- src/main/java/com/project/blog/entities/Post.java
+++ src/main/java/com/project/blog/entities/Post.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,4 +33,9 @@ public class Post {
 	private String imageName;
 	
 	private Date addedDate;
+	
+	@ManyToOne
+	private Category category;
+	@ManyToOne
+	private User user;
 }
