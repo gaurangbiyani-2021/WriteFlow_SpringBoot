c2ef9f9fcab269c6b9c1c3ff4cb023659eb04001
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-12-25 21:01:23 +0530

    post service api

---
+++ src/main/java/com/project/blog/payloads/PostDto.java
@@ -0,0 +1,6 @@
+package com.project.blog.payloads;
+
+public class PostDto {
+	private String title;
+	private String 	content;
+}
---
+++ src/main/java/com/project/blog/services/PostService.java
@@ -0,0 +1,29 @@
+package com.project.blog.services;
+
+import java.util.List;
+
+import com.project.blog.entities.Post;
+import com.project.blog.payloads.PostDto;
+
+public interface PostService {
+	
+	//create posts 
+	Post createPost(PostDto postDto);
+	
+	//update posts
+	Post updatePost(PostDto postDto, Integer postId);
+	
+	void deletePost(Integer postId);
+	
+	// get all posts 
+	List<Post> getAllPost();
+	
+	//get single post 
+	Post getPostById(Integer postId);
+	
+	// get all posts by category 
+	List<Post> getPostsByCategory(Integer categoryId);
+
+	// get all posts by user
+	List<Post> getPostsByUser(Integer userId);
+}
