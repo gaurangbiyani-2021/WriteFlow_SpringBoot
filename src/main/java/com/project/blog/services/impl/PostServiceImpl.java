08133611d3040354e791b1d3d082b6bffd23bc53
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-12-26 14:12:10 +0530

    post service error resolved

--- src/main/java/com/project/blog/services/impl/PostServiceImpl.java
+++ src/main/java/com/project/blog/services/impl/PostServiceImpl.java
@@ -80,7 +80,7 @@ public class PostServiceImpl implements PostService {
 		Category cat = this.categoryRepo.findById(categoryId).orElseThrow(IllegalArgumentException::new);
 		List<Post> posts = this.postRepo.findByCategory(cat);
 		
-		List<PostDto> postDtos = posts.stream().map((post)->this.modelMapper.map(posts,PostDto.class)).collect(Collectors.toList());
+		List<PostDto> postDtos = posts.stream().map((post)->this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
 		return postDtos;
 	}
 
@@ -91,7 +91,7 @@ public class PostServiceImpl implements PostService {
 		User user = this.userRepo.findById(userId).orElseThrow(IllegalArgumentException::new);
 		List<Post> posts = this.postRepo.findByUser(user);
 		
-		List<PostDto> postDtos = posts.stream().map((post)->this.modelMapper.map(posts,PostDto.class)).collect(Collectors.toList());
+		List<PostDto> postDtos = posts.stream().map((post)->this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
 		return postDtos;
 	
 	}
