78f69a4028adac6ff505e019fabfafefb13bf334
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2024-01-01 15:04:17 +0530

    comments mapping with post done

--- src/main/java/com/project/blog/entities/Post.java
+++ src/main/java/com/project/blog/entities/Post.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -45,6 +46,6 @@ public class Post {
 	
 	private User user;
 	
-	@OneToMany
+	@OneToMany(mappedBy = "post" , cascade = CascadeType.ALL)
 	private Set<Comment>comments = new HashSet<>();
 }
