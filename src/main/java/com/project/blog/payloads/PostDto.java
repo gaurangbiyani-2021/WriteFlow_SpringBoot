04395273e71a0124a1fda821ba357f7e5727e7fc
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2024-01-01 17:12:56 +0530

    comment repo

--- src/main/java/com/project/blog/payloads/PostDto.java
+++ src/main/java/com/project/blog/payloads/PostDto.java
@@ -1,8 +1,11 @@
 package com.project.blog.payloads;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.project.blog.entities.Category;
+import com.project.blog.entities.Comment;
 import com.project.blog.entities.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -28,4 +31,6 @@ public class PostDto {
 	private Date addedDate;
 	private CategoryDto category;
 	private UserDto user;
+	
+	private Set<Comment> comments = new HashSet<>();
 }
