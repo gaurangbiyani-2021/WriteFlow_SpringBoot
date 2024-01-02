551e8f3f9b63952da3fdecd6d0e13567b45e0b57
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2024-01-02 16:30:33 +0530

    jwt

---
+++ src/main/java/com/project/blog/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.project.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+}
