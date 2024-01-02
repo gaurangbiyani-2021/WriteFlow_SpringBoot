48a63c93a455aef75bd0a1b5a1e62f45b9ff9d79
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2024-01-02 13:56:27 +0530

    comment service api impl

--- src/main/java/com/project/blog/services/impl/CommentServiceImpl.java
+++ src/main/java/com/project/blog/services/impl/CommentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.project.blog.services.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.project.blog.entities.Comment;
 import com.project.blog.entities.Post;
 import com.project.blog.payloads.CommentDto;
 import com.project.blog.repositories.CommentRepo;
@@ -14,18 +16,27 @@ public class CommentServiceImpl implements CommentService {
 	private PostRepo postRepo;
 	@Autowired
 	private CommentRepo commentRepo;
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public CommentDto createComment(CommentDto commentDto, Integer postId) {
 		// TODO Auto-generated method stub
 		Post post = this.postRepo.findById(postId).orElseThrow(IllegalArgumentException::new);
-		return null;
+		Comment comment = this.modelMapper.map(commentDto,Comment.class);
+		
+		comment.setPost(post);
+		Comment savedcomment = this.commentRepo.save(comment);
+		
+		return this.modelMapper.map(savedcomment, CommentDto.class);
 	}
 
 	@Override
 	public void deleteComment(Integer commentId) {
 		// TODO Auto-generated method stub
 
+		Comment com = this.commentRepo.findById(commentId).orElseThrow(IllegalArgumentException::new);
+		this.commentRepo.delete(com);
 	}
 
 }
